36278688ca9bdad9671012b39f0663f55d954f0d
Giles Bowkett <gilesb@gmail.com>
2011-06-10 18:11:26 -0700

    comments

---
+++ default.js
@@ -0,0 +1,5 @@
+// I want to use this file to do a hack version of RescueTime.
+
+// probably I'll issue a GET against a non-existent gif, return a 1x1 invisipixel, and use the
+// specifics of the phony request on the server end to ascertain the URL and the timestamp.
+
