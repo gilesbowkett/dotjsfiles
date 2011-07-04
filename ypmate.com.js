3814d69d78aa5b50a8adb19c3f5deb2edc795c35
Giles Bowkett <gilesb@gmail.com>
2011-07-03 20:33:04 -0700

    better handled via the hosts file

--- ypmate.com.js
+++
@@ -1,4 +0,0 @@
-// this site is a horrible porn popup that gets around Chrome's popup blocking somehow. or
-// maybe I forgot to enable it, or Chrome disabled it during an update - no idea. anyway,
-// this is a desperation fix. TODO: come up with a better fix
-window.location="http://wikipedia.org"
