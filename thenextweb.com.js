c3aa4187ceee582764f118d6c8114152b53428c2
Giles Bowkett <gilesb@gmail.com>
2011-07-05 16:06:44 -0700

    just got started on this one
    these fuckers are almost as bad as yahoo

---
+++ thenextweb.com.js
@@ -0,0 +1,6 @@
+$("<style type='text/css'>.lf_comment_notifier{ display: none }</style>").appendTo("head");
+$(".sidebar").hide();
+$(".content-sidebar").hide();
+$(".topmenu").hide();
+$(".banner-wrapper").hide();
+$(".header").hide();
