02299c14854a08ab7589d332048721e82bac8728
Giles Bowkett <gilesb@gmail.com>
2011-07-05 21:31:32 -0700

    fuck facebook

---
+++ facebook.com.js
@@ -0,0 +1,7 @@
+$("#rightCol").hide();
+$("#pagelet_home_stream").css("width", "600px");
+$("groupSideNavWrapper").hide();
+$("#pagelet_apps_nav").hide();
+$(".fbDock").hide();
+$("<style type='text/css'>.fbDock{ display: none }</style>").appendTo("head");
+
