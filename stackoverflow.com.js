305e8062e39bfb8adf6c3eb279d3960120ad767c
Giles Bowkett <gilesb@gmail.com>
2011-07-07 09:46:54 -0700

    more ad removal, plus comments

--- stackoverflow.com.js
+++ stackoverflow.com.js
@@ -1,5 +1,12 @@
-$(".hireme").hide();
-$("#adzerk1").hide();
+// too many nav options
 $(".mainnavs ul").hide();
 $("#custom-header").hide();
+$("#hlinks").hide();
+
+// ads
 $("#portalLink").hide();
+$(".hireme").hide();
+$("#adzerk1").hide();
+$(".everyonelovesstackoverflow").hide();
+$("#adzerk2").hide();
+
