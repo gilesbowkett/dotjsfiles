04c9c91b73a6276e4ec919371e77616b3d56092a
Giles Bowkett <gilesb@gmail.com>
2011-07-07 12:45:38 -0700

    only add "Network" to "Graphs" tab's menu

--- github.com.js
+++ github.com.js
@@ -9,6 +9,8 @@ $(".sidebar").hide()
 var link_to_network_graph = $("ul.tabs li:contains('Network') a").attr("href");
 
 if ("" != link_to_network_graph) {
-  $(".subnav-bar ul").append("<li><a href='" + link_to_network_graph + "'>Network</a></li>")
+  if ($("ul.tabs li:contains('Graphs') a").hasClass("selected")) {
+    $(".subnav-bar ul").append("<li><a href='" + link_to_network_graph + "'>Network</a></li>")
+  }
 }
 
