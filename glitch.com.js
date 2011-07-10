2d8eb4f391b383fe4f00518fc26de418dd5df044
Giles Bowkett <gilesb@gmail.com>
2011-07-10 15:25:06 -0700

    readability

---
+++ glitch.com.js
@@ -0,0 +1,5 @@
+// this is by far the most common modification I need to make. only thing
+// that changes is the CSS selectors. may DRY this up soon...
+
+$(".aside").hide();
+$(".main").css("margin-left", "20px");
