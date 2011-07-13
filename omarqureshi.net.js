ba46ee79fd4312b4ba5f4d166809cc48eee010ba
Giles Bowkett <gilesb@gmail.com>
2011-07-12 19:28:44 -0700

    wow. just WOW.

---
+++ omarqureshi.net.js
@@ -0,0 +1,8 @@
+// this guy's post on postgresql tsearch2 is good, but his CSS easily wins the grand
+// prize for WTFs per LOC I've ever seen (CSS division), and his spelling's awful too
+
+$(".inner-wrapper").css("padding", "0");
+$(".quote").hide();
+$(".suffix-one").css("margin", "0");
+$(".eight-col").css("width", "617px");
+$(".highlight-wrapper").css("margin-left", "0"); // seriously, wtf were you thinking?
