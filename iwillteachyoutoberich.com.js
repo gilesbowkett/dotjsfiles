d2c1372579ebed1e3d910ead0a00334a084e9132
Giles Bowkett <gilesb@gmail.com>
2011-08-03 11:33:22 -0700

    admitting defeat, simply not using site

--- iwillteachyoutoberich.com.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $(".lightbox").hide();
-  $("<style type='text/css'>.lightbox{ display: none }</style>").appendTo("head");
-
-  $("#email-lightbox").hide();
-  $("<style type='text/css'>#email-lightbox{ display: none }</style>").appendTo("head");
-});
-
