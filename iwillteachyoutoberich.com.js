2c03d0ae1c6eeb51c3e44c9e299591fa35f3af8d
Giles Bowkett <gilesb@gmail.com>
2011-08-03 11:33:06 -0700

    fucked

--- iwillteachyoutoberich.com.js
+++ iwillteachyoutoberich.com.js
@@ -1,5 +1,8 @@
 $(function() {
   $(".lightbox").hide();
   $("<style type='text/css'>.lightbox{ display: none }</style>").appendTo("head");
+
+  $("#email-lightbox").hide();
+  $("<style type='text/css'>#email-lightbox{ display: none }</style>").appendTo("head");
 });
 
