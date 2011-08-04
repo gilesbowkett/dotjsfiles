c7d6d58422abb3cf5523c6b499543eebf6c91da2
Giles Bowkett <gilesb@gmail.com>
2011-08-03 23:53:02 -0700

    wtf but it works but wtf

---
+++ theatlantic.com.js
@@ -0,0 +1,5 @@
+$("<style type='text/css'>.contentColumn{ margin-left: 20px; }</style>").appendTo("head");
+
+// I have NO FUCKING IDEA why the above is necessary. I think theatlantic.com may be doing
+// something odd, unusual, and/or highly ill-advised
+
