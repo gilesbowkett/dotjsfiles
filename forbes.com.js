4faa2887a867d406875a45c2380fc7201fbf3423
Giles Bowkett <gilesb@gmail.com>
2011-10-19 07:58:30 -0700

    forbes.com now essentially readable

---
+++ forbes.com.js
@@ -0,0 +1,12 @@
+$("header").hide()
+$("#leaderboardad").hide()
+$(".user_block").hide()
+$(".no_comment_bugs_exist_yet").hide() // srsly?
+$("aside").hide()
+
+$("hgroup").css("margin-left", "20px")
+$(".article").css("margin-left", "20px")
+
+$(".engagement_block").hide()
+$("#comments").hide()
+$("footer").hide()
