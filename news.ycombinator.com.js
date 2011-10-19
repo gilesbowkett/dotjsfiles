bd4b09678ef244071a9d9e1301a2f9590d0b79ae
Giles Bowkett <gilesb@gmail.com>
2011-10-19 08:21:20 -0700

    made hacker news comments legible
    not readable, only legible

---
+++ news.ycombinator.com.js
@@ -0,0 +1,7 @@
+$("table").attr("bgcolor", "#ffffff") // srsly?
+$("td.default").css("background-color", "#fffffc");
+$("td.default").css("padding-bottom", "20px");
+$("td.default").css("border", "1px solid #fffffa");
+
+$("span.comment").css("font-family", "Trebuchet MS");
+$("span.comment").css("line-height", "1.6em");
