fdea6a8286128639ecab90f1ee9fc644b1b621b3
Giles Bowkett <gilesb@gmail.com>
2012-02-04 16:45:49 -0800

    kludge :-p

--- news.tubefilter.tv.js
+++ news.tubefilter.tv.js
@@ -1,1 +1,15 @@
-$(".postcontent").css("margin-left", "20px");
+_TubefilterDotJS = {}
+
+$(document).ready(function() {
+
+  // copied from the aggravating Wired hack (wired.com.js)
+
+  _TubefilterDotJS.removeAllKindsOfAggravatingUnnecessaryCrapFromTubefilter = function() {
+    $(".singlesidebar").hide()
+    $(".singlepost").css("margin-left", "35px")
+  }
+
+  setTimeout(_TubefilterDotJS.removeAllKindsOfAggravatingUnnecessaryCrapFromTubefilter, 1000);
+
+});
+
