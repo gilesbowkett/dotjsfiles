8098d078fe4dc22644057ad1f1bd907ce9b9da2f
Giles Bowkett <gilesb@gmail.com>
2012-02-17 22:43:30 -0800

    no sidebar, LEFT MARGIN READ ROBERT BRINGHURST WTF
    plus a little bit of random distraction removal

---
+++ io9.com.js
@@ -0,0 +1,8 @@
+// this is probably usable as a fix for all Gawker Network blogs
+
+$("#rightcontainer").remove()
+$("#rightwrapper").remove()
+$("#rightbar").remove()
+$(".inner").css("margin-left", "20px");
+setTimeout(function(){$(".post-supp").hide()}, 1000)
+
