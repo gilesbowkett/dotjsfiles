8ba9d226d130c0794a08cd97ba74051fcddf8bc7
Giles Bowkett <gilesb@gmail.com>
2012-03-06 11:52:39 -0800

    sanitation

--- astrologyzone.com.js
+++ astrologyzone.com.js
@@ -1,6 +1,9 @@
+// embarassing to have this in my repo, but it should still be readable
+
 $(document).ready(function() {
   var text = $("span.text")[0];
   $("body").html(text);
   $("body").css("margin-left", "20px");
+  $("p").css("width", "80%");
   $("img").remove();
 });
