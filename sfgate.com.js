1e8aa22173746a87bf8269577a5280c5e0acccbd
Giles Bowkett <gilesb@gmail.com>
2012-05-26 16:04:16 -0700

    more astrology bullshit
    not proud of reading these things, but by God if I'm going to read
    them, I'll do it without the unbelievable UX nightmare that is
    sfgate.com

---
+++ sfgate.com.js
@@ -0,0 +1,8 @@
+// embarassing to have this in my repo, but it should still be readable
+
+$(document).ready(function() {
+  var text = $(".horoscope_listings");
+  $("body").html(text);
+  $("body").css("margin-left", "20px");
+  $("body").css("margin-top", "20px");
+});
