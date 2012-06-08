b70f9c36660066056a4015cca48a27a0c8d7274c
Giles Bowkett <gilesb@gmail.com>
2012-06-07 19:27:32 -0700

    this will very probably happen for all NYT
    likewise with their paywall. I've encountered it once, and it
    seems to consist of nothing but a lightbox-style overlay. it will
    very probably be trivial to circumvent

---
+++ fivethirtyeight.blogs.nytimes.com.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $("aside").hide();
+  $("<style type='text/css'>.nytdGrowlNotify{ display: none }</style>").appendTo("head");
+  $("<style type='text/css'>.nytdGrowlUrgent{ display: none }</style>").appendTo("head");
+  $("<style type='text/css'>.nytdGrowlUIContainer{ display: none }</style>").appendTo("head");
+});
+
