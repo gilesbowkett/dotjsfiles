8b3b6faadf9da1899719f4043c08f52aa4935e81
Giles Bowkett <gilesb@gmail.com>
2012-10-31 10:43:33 -0700

    sadly, this broke facebook
    so I've had to go back to using Facebook's broken UI :-p

--- facebook.com.js
+++
@@ -1,9 +0,0 @@
-$("#pagelet_home_stream").css("width", "600px");
-$("groupSideNavWrapper").hide();
-$("#pagelet_apps_nav").hide();
-$(".fbDock").hide();
-$("<style type='text/css'>.fbDock{ display: none }</style>").appendTo("head");
-$("<style type='text/css'>#rightCol{ display: none }</style>").appendTo("head");
-
-$("#pagelet_sidebar").remove()
-
