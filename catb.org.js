0ae95b3246768c7d76ef9a6f3f7a3e1323af8f93
Giles Bowkett <gilesb@gmail.com>
2013-01-15 17:24:05 -0800

    chapter 1 section 6 is legible also now
    I know it was written in 2003, but that is some utterly awful
    HTML.

--- catb.org.js
+++ catb.org.js
@@ -1,6 +1,9 @@
 $("body").css("background-color", "white").
           css("background-image", "none");
 
-$(".book").css("width", "80%").
-           css("margin-left", "5%");
+// I can't believe the motherfucker who wrote this book also wrote this HTML
+$.each([".book", ".sect1"], function(index, element) {
+  $(element).css("width", "80%").
+             css("margin-left", "5%");
+});
 
